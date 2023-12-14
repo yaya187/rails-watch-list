f658e9468d16736fc597b5297f452b6062b6ae0a
yaya <y.niang0@laposte.net>
2023-12-15 00:06:47 +0100

    front-list-movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,15 @@ url = 'http://tmdb.lewagon.com/movie/top_rated'
 movies_serialized = URI.open(url).read
 movies = JSON.parse(movies_serialized)
 
+puts "Clearing database..."
+Movie.destroy_all
+
 movies["results"].each do |movie|
   title = movie["title"]
   overview = movie["overview"]
   poster_url = "https://image.tmdb.org/t/p/w500#{movie['poster_path']}"
   rating = movie["vote_average"]
 
-  Movie.create(title: title, overview: overview, poster_url: poster_url, rating: rating)
+  new_movie = Movie.create!(title: title, overview: overview, poster_url: poster_url, rating: rating)
+  puts "Created #{new_movie.title}"
 end
