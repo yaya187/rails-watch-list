334c4e4f45adc9902dba2b6e80cb9546a2051c98
yaya <y.niang0@laposte.net>
2023-12-15 16:07:23 +0100

    front-index-new-show

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,9 +1,14 @@
 class BookmarksController < ApplicationController
   def new
-    @list = List.find(params[:list_id])
-    @bookmark = Bookmark.new
+    begin
+      @list = List.find(params[:list_id])
+      @bookmark = Bookmark.new
+    rescue ActiveRecord::RecordNotFound
+      # Si la liste n'est pas trouvée, rediriger vers une autre page avec un message d'erreur
+      flash[:alert] = "La liste demandée n'a pas été trouvée."
+      redirect_to root_path # Remplacez 'some_other_path' par le chemin de redirection souhaité
+    end
   end
-
   def create
     @bookmark = Bookmark.new(bookmark_params)
     @list = List.find(params[:list_id])
